527949ad928be9af33316fc69bbf7a619188cc8a
CyberWarmonger <CyberWarmonger@CYBERWARMONGER>
2024-04-06 19:27:56 +0300

    Kategorilere Ait Yemek Listesi ve Banner

--- KategoriDetay.aspx.cs
+++ KategoriDetay.aspx.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
 namespace YemekTarif
 {
     public partial class KategoriDetay : System.Web.UI.Page
     {
+        sqlsinif snf = new sqlsinif();
+        string kategoriid = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            kategoriid = Request.QueryString["Kategoriid"];
+            SqlCommand komut = new SqlCommand("select*from tbl_yemekler where kategoriid=@p1",snf.baglanti());
+            komut.Parameters.AddWithValue("@p1", kategoriid);
+            SqlDataReader dr = komut.ExecuteReader();
+            DataList2.DataSource = dr;
+            DataList2.DataBind();
         }
     }
 }
